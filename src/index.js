e364af1ab97bf18b3a92cc75a3f3024ca07d7db4
hai <2516649312@qq.com>
2021-04-18 09:50:00 +0800

    feat:实现位置定位#rtc#1818188

--- src/index.js
+++ src/index.js
@@ -1,78 +1,180 @@
 require("./style/comme.sass");
 import { cell } from "./utils/cell_factory";
+import { base_cell } from "./cell/cell_render.js";//
 
 
-export const component_flow = class {
-  constructor({
-    ele ,     // 
+const component_flow = class {
+  constructor(options){
+    this.initData(options)
+    this.initPage(); // 初始化画布；
+  }
+
+  initData({ 
+    target,   //s vg || document || cavance
+    ele,      // 
     flow_Data // 渲染数据
   }){
+    this.target = target;
+    this.renderCellTypes = [];
     this.ele = document.getElementById(ele);
-
-    const fib = ( flow_Data, deep )=>{ // 递归使用 cell 对象包装 原始对象 
-      flow_Data.map((item ,i)=>{
-        if( Object.prototype.toString(item) === '[object Object]'){ // 如果他是一个对象的话--直接使用对象包装他
-          item.id = deep + '__' + i;
-          item.__self = item; // 便于拿到自己
-          return {
-            ...cell,
-            ...item
-          };
-        }
-
-        if( Object.prototype.toString(item) === '[object Object]' ){
-          return fib(item, deep + '__' + i);
-        }
-
-        if( typeof item === 'String' ){
-          const cell_ = { 
-            text: item,
-            id: deep + '__' + i
-          };
-          cell.__self = self;
-
-          return cell_;
-        }
-      });
-    };
     
-    // 初始化数据
-    this.renderData = fib(flow_Data, '0');
-
-    this.initPage(); // 初始化画布；
-
-    this.regietser();
-    
+    this.renderData = this.mapData((data, deep, parentTop)=> {
+      data.id =   '__' + Math.ceil(Math.random() * 1000 *1000 * 1000);
+      data.__self = data;
+      Object.defineProperties(data,Object.getOwnPropertyDescriptors(cell));
+      data.deep = deep; // 重写属性
+      data.topArr = parentTop;  // 重写属性
+      // console.log('data' , data, deep, parentTop);
+      return data;
+    }, flow_Data);
   }
 
   /**
    * 注册画布
-   */
+   */  
   initPage(){
+    const svgStr = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">'+
 
+                  '</svg';
+    const html = new DOMParser().parseFromString(svgStr, "text/xml");
+    this.svgPage = html.getElementsByTagName('svg')[0];
+
+    this.ele.appendChild(this.svgPage);
+    console.log( this.svgPage );
+  }
+
+  // 总体注册
+  regietser({type,content}){
+    const mapAction = {
+      cell: ()=> { this.regietser_cell( content ) },
+      line: ()=> { this.regietser_line( content ) }
+    };
+    mapAction[type]();
+  }
+
+  // 注册单元格
+  regietser_cell( content ){
+    if( Object.prototype.toString(content) === '[object Object]' ){
+      if( !content.render_type && this.target !== 'svg' ){
+        console.warn('cell 的渲染对象需要定义为 svg、document、或cavance');
+      }else{
+        this.renderCellTypes.push( content );
+      };
+    }else  if( Object.prototype.toString(content) === '[object Arrary]' ){
+      this.renderCellTypes = [ ...this.renderCellTypes, ...content ];
+    }else{
+      console.warn('cell 类型定义不正确，你需要定义一个cell对象或者cell对象数组');
+    }
   }
 
   render () {
-    
-    
-  }
-  
-  // ({
-  //   eleId, // 绑定的id对象
-  //   flow_Data, // 渲染的数据
-  // })=>{
-  //   const ele = document.getElementById(eleId);
+    // 选择渲染的单元格
+    const cell_templete = this.renderCellTypes[0];
 
-  //   return {
-  //     ele,
-  //   }
-  // }
+    // 为每个单元格添加函数对象
+    this.renderData = this.mapData(( data )=> {
+      Object.assign(data, cell_templete);
+      setTimeout(()=> {
+        console.log('newData',data, data.top, data._child_max_width );
+      }, 200);
+
+      // data._child_max_width;
+      if(data.svg && data.render_type === 'svg') {
+        // var html  = new DOMParser().parseFromString(newData.render(newData), "text/xml");
+        // newData.svg = html.getElementsByTagName('svg');
+        // debugger;
+        // console.log(newData.svg);
+        // console.log(newData);
+      }
+
+      return data;
+    }, this.renderData);
+
+    this.renderCell();
+  }
+
+  renderCell(){
+    this.mapData((item)=> {
+      const { svg, left, top } = item;
+      svg.setAttribute('x', left);
+      svg.setAttribute('y', top);
+      this.svgPage.appendChild(svg);
+      return item;
+    }, this.renderData)
+  }
+
+
+  /**
+   * 
+   * @param {数据处理函数；你需要返回一个数据的包装对象} func 
+   * @param {this.mapData} data 
+   * @param  {...any:需要传入func的参数} otherParams 
+   * @returns 
+   */
+  mapData (func, data, deep = 0, parentTop = [], ...otherParams){
+    if( !data ) return;
+    if( Object.prototype.toString.call(data) === '[object Object]'  ){
+      const obj  = func(data, deep, parentTop,...otherParams);
+      if( obj.children ){
+        obj.children = this.mapData(func, obj.children,  deep + 1, parentTop, ...otherParams);
+      }
+      return obj;
+    }else if( Object.prototype.toString.call(data) === '[object Array]' ){
+      const arr = [];
+      return data.map( (item ,index )=> {
+        const res =  this.mapData(func, item,deep , parentTop.concat(arr), ...otherParams);
+        arr.push(item);
+        return res;
+      }) 
+    }
+    return data;
+  }
 
   bindEvent(){
 
   }
-
-  // Life: {
-    
-  // }
 }
+
+const flow = new component_flow({
+  ele: 'app',
+  flow_Data: [
+    {
+      label: '100',
+      // type: '',
+      children: [
+        {
+          label: '200',
+        },
+        {
+          label: '201',
+          children: [
+            {
+              label: '300',
+
+              children: [
+                {
+                  label: '300'
+                },
+                {
+                  label: '301'
+                },
+              ]
+            },
+            {
+              label: '301'
+            },
+          ]
+        },
+        {
+          label: '202'
+        },
+      ]
+    }
+  ]
+});
+
+flow.regietser({ type: 'cell', content: base_cell });
+
+flow.render();
+
+console.log(flow, flow.renderData, flow.renderCellTypes );
--- src/utils/cell_factory.js
+++ src/utils/cell_factory.js
@@ -1,36 +1,96 @@
 // 定时数据的共同行为
+// _表示不希望向外暴露的属性即私有属性
 export const cell = {
   id: '',
-  left: '',
-  right: '',
+  deep: 0,
+  topArr: [],
+  top: '',
+  left: 0,
+  right: 0,
   width: '',
-  height: '',
-  text: '',
-  svg: '',
-  render_type: '',
-  // render: '', // String
-}
+  height: 0,
+  render: '',
+  _render:undefined,
+  svg: undefined,
+  // _child_max_height: '', // 子孩子中最大的宽度
+  cell_type: 'base_cell',
+};
 
-Object.defineProperty( cell , 'svg', {
-  get : ()=>{
-   if( cell.render ){
-     var svg = new DOMParser().parseFromString(cell.render, "text/xml");
-     return svg;
-   }
-   return null;
+Object.defineProperty(cell, 'render',{
+  set: function(render_func){
+    if( render_func && Object.prototype.toString.call(render_func) === '[object Function]' ){
+      const html = new DOMParser().parseFromString(render_func(this), "text/xml");
+      const svg = html.getElementsByTagName('svg')[0];
+      this.svg = svg;
+      this.width = this.svg.getBBox().width || this.svg.width.baseVal.value;
+      this.height = this.svg.getBBox().height || this.svg.height.baseVal.value;
+      // this._child_max_height = this.width;
+    }
+    this._render = render_func;
   }
 })
 
-// Object.defineProperty( cell , 'width', {
-//   get : ()=>{
-//    if( cell.render ){
-//      var svg = new DOMParser().parseFromString(cell.render, "text/xml");
-
-//      return svg.
-//    }
-//    return 0;
+// Object.defineProperty( cell , 'svg', {
+//   enumerable: true,
+//   get : function(){
+//     if( this._svg ){
+//       return this._svg
+//     };
+//     // debugger;
+//     if( this.render && Object.prototype.toString.call(this.render) === '[object Function]'){
+//       const html = new DOMParser().parseFromString(this.render(this), "text/xml");
+//       const svg = html.getElementsByTagName('svg')[0];
+//       if( svg ){
+//         this._svg = svg;
+//         // this.width = this._svg.getBBox().width || this._svg.width.baseVal.value;
+//         this.height = this._svg.getBBox().height || this._svg.height.baseVal.value;
+//       }
+//       return svg;
+//     }
+//     return undefined;
 //   }
-// })
+// });
+
+// Object.defineProperty( cell , 'width', {
+//   enumerable: true,
+//   get : function(){
+//     if( this._svg ){
+//       return this._svg.getBBox().width || this._svg.width.baseVal.value;
+//     };
+//     return 0;
+//   }
+// });
+
+Object.defineProperty( cell , '_child_max_height', {
+  enumerable: true,
+  get : function(){
+    if( this.children ){
+      return this.children.reduce((pre,next)=>{
+        return pre + ( next._child_max_height || next.height)
+      },0);
+    };
+    return this.height;
+  }
+});
+
+Object.defineProperty( cell , 'top', {
+  enumerable: true,
+  get : function(){
+    const top_arr_width = this.topArr.reduce((pre,next)=> {
+      return pre + next._child_max_height
+    },0);
+    return Math.floor((this._child_max_height   - this.height)  / 2 + top_arr_width); 
+  }
+});
+
+Object.defineProperty( cell , 'left', {
+  enumerable: true,
+  get : function(){
+    return this.deep * ( this.width )
+  }
+});
+
+
 
 export const line = {
   from: [],
